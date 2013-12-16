bcde5f1d1f9ed3de0f2a1c5e1cff6b3bc016a2b2
Jasper <jasperlin451@gmail.com>
2013-12-16 03:47:55 -0500

    working code

--- excelImport.py
+++ excelImport.py
@@ -1,5 +1,18 @@
+#import data from worksheets
+#xlrd package needs to be downloaded
+#function takes in document name (looking to eventual steps towards gui)
+#data is scanned into 2D list, first item of each list is the title of the column
+
 def scan(name):
 	import xlrd
 	workbook=xlrd.open_workbook(name)
 	sheet=workbook.sheet_by_name('Sheet1')
-	return(sheet.cell_value(5,5))
+	num_rows=sheet.nrows
+	num_cells=sheet.ncols
+	data=[[] for i in range(num_cells-1)]
+	for curr_cell in range(num_cells-1):
+		temp=[]
+		for curr_row in range (num_rows-1):
+			temp.append(sheet.cell_value(curr_row,curr_cell))
+		data[curr_cell]=temp	
+	return(data)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#import excel file, print data
 import excelImport
 a=excelImport.scan('/home/jasper/Downloads/data.xls')
 print(a)
