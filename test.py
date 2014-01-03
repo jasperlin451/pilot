d93301eb97c0eccf97a8fee261b6e25dccdb069f
Jasper <jasperlin451@gmail.com>
2014-01-03 02:13:53 -0500

    combo

--- test.py
+++ test.py
@@ -4,11 +4,17 @@ a,b = studentImport.scan('/home/jasper/pilot/data.xls','Calc II')
 #for i in range(len(a)):
 #    print(a[i].avail)
 #print(' ')
+import combinations
+combos=combinations.combinations(b,8,a)
+print(len(combos))
+f=open('output','r+')
+for e in range(len(combos)):
+    f.write(str(combos[e])+'\n')
 
-import importRooms
-rooms=importRooms.scan('/home/jasper/pilot/rooms.xlsx')
-for i in range(len(rooms)):
-    print(rooms[i].time, rooms[i].classroom, rooms[i].group)
-import sort
-[c,d,e]=sort.checkRoom(rooms,'Monday 5 - 7 pm')
-print(c,d,e)
+#import importRooms
+#rooms=importRooms.scan('/home/jasper/pilot/rooms.xlsx')
+#for i in range(len(rooms)):
+#    print(rooms[i].time, rooms[i].classroom, rooms[i].group)
+#import sort
+#[c,d,e]=sort.checkRoom(rooms,'Monday 5 - 7 pm')
+#print(c,d,e)
