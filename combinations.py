7fca9eeb45bbc9ee5e865fde57158a7bb4ae6f6c
Jasper <jasperlin451@gmail.com>
2014-01-03 02:13:02 -0500

    combo

--- combinations.py
+++ combinations.py
@@ -3,8 +3,29 @@
 #possibleTimes is scanned in from each subject as is numberofClasses
 import itertools
 
-def combinations(possibleTimes,numberofClasses):
+def combinations(possibleTimes,numberofClasses,classData):
     combo=[]
     for c in itertools.combinations_with_replacement(possibleTimes,numberofClasses):
-        combo.append(c)
+        a=checkStudents(classData,c,possibleTimes)
+        if a==1:
+            combo.append(c)
     return(combo)
+
+#returns time slots and leader meeting days
+
+def checkStudents(data,combination,possibleTimes):
+    temp=data
+    for i in range(len(combination)):
+        index=possibleTimes.index(combination[i])
+        temp=sorted(temp,key=lambda student: student.avail[index])
+        #remove the first 10 from list if 1 or 2
+        #meaning a class can be made
+        try:
+            x=temp[9].avail[index]
+            if x==3:
+                return(0)
+            else:
+                del temp[0:10]
+        except(IndexError):
+             return(0)
+    return (1)
